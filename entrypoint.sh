2cd73ea4e61ccda064b1a6f697bfa3b944c5d88e
Kittycat7J <167596510+Kittycat7J@users.noreply.github.com>
2026-03-11 13:19:49 -0400

    Remove unnecessary curl commands from entrypoint.sh
    Removed redundant curl commands for fetching config files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,6 @@ cd /home/container
 # Output Node version
 node --version
 
-# Create config from environment variables or use existing config.json
-curl -O https://github.com/Kittycat7J/Vanilla-Chatbridge/raw/refs/heads/main/index.js
-curl -O https://github.com/Kittycat7J/Vanilla-Chatbridge/raw/refs/heads/main/entrypoint.sh
-curl -O https://github.com/Kittycat7J/Vanilla-Chatbridge/raw/refs/heads/main/package.json
-curl -O https://github.com/Kittycat7J/Vanilla-Chatbridge/raw/refs/heads/main/package-lock.json
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
